253dc3ed16bf8c6dda51be40d8b89068cb4c15ca
Heron Mia <howa.anil@gmail.com>
2025-11-25 23:10:01 +0600

    js

--- first.js
+++ first.js
@@ -2,28 +2,27 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
-  if (req === "/") {
+  if (req.url === "/") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>Coding1</title></head>");
     res.write("<body><h1>Welcome</h1></body>");
     res.write("</html>");
-    res.end();
-  } else if (req === "/products") {
+    return res.end();
+  } else if (req.url === "/products") {
     res.setHeader("Content-Type", "text/html");
     res.write("<html>");
     res.write("<head><title>Coding2</title></head>");
     res.write("<body><h1>Learning2</h1></body>");
     res.write("</html>");
-    res.end();
-  } else {
-    res.setHeader("Content-Type", "text/html");
-    res.write("<html>");
-    res.write("<head><title>Coding3</title></head>");
-    res.write("<body><h1>Learning3</h1></body>");
-    res.write("</html>");
-    res.end();
+    return res.end();
   }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Coding3</title></head>");
+  res.write("<body><h1>Learning3</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 const PORT = 4001;
---
+++ user.js
@@ -0,0 +1,39 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method, req.headers);
+  if (req.url === "/") {
+    res.setHeader("Content-Type", "text/html");
+    res.write("<html>");
+    res.write("<head><title>Coding1</title></head>");
+    res.write("<body><h1>Enter Your Details :</h1>");
+    res.write("<form action='/submit-details' method:'POST'>");
+    res.write(
+      "<input type='text' name='username' placeholder='Enter You Name'> <br><br>"
+    );
+    res.write("<label for='gender'>Gender: </label>");
+    res.write("<label for='male'>Male: </label>");
+    res.write("<input type='radio' name='gender' id='male' value='male'>");
+    res.write("<label for='female'>Female: </label> ");
+    res.write(
+      "<input type='radio' name='gender' id='female' value='female'> <br><br>"
+    );
+    res.write("<input type='submit' value='Submit' >");
+    res.write("</form>");
+    res.write("</body>");
+    res.write("</html>");
+    return res.end();
+  }
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>Coding3</title></head>");
+  res.write("<body><h1>Learning3</h1></body>");
+  res.write("</html>");
+  res.end();
+});
+
+const PORT = 4001;
+
+server.listen(PORT, () => {
+  console.log(`Server running http://localhost:${PORT}`);
+});
